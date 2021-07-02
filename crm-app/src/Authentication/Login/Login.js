003a03491f91a828039afbc3128f4e7fa800397e
thomdela2 <thomdela2@student.arteveldehs.be>
2021-07-02 16:51:41 +0200

    🎉 Initialized CRM App

---
+++ crm-app/src/Authentication/Login/Login.js
@@ -0,0 +1,13 @@
+import './Login.css';
+
+function Login() {
+  return (
+    <div>
+      <div>
+        <p>Login</p>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
---
+++ crm-app/src/Authentication/Register/Register.js
@@ -0,0 +1,13 @@
+import './Register.css';
+
+function Register() {
+  return (
+    <div>
+      <div>
+        <p>Register</p>
+      </div>
+    </div>
+  );
+}
+
+export default Register;
